327d2252c539e30d43a3425f020956bbdd318b5e
Natan <semnomemonmes@gmail.com>
2017-07-07 14:59:41 -0300

    RateLimits

--- src/main/java/gabrielbot/utils/http/Webhook.java
+++ src/main/java/gabrielbot/utils/http/Webhook.java
@@ -9,6 +9,9 @@ import net.dv8tion.jda.core.entities.impl.MessageImpl;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.time.OffsetDateTime;
+import java.util.concurrent.TimeUnit;
+
 public class Webhook {
     public static final String API_ENDPOINT = "https://canary.discordapp.com/api/webhooks/%s/%s"; //id and token
     public static final Requester REQUESTER = new Requester();
@@ -61,13 +64,40 @@ public class Webhook {
         return post(new MessageBuilder().append(message).build());
     }
 
-    private static final class Requester extends HTTPRequester {
+    public static final class Requester extends HTTPRequester {
+        private boolean configured = false;
+
         Requester() {
-            super("WebhookRequester", new RateLimiter(5, 5000));
+            super("WebhookRequester", new RateLimiter(4, 5000), builder->
+                builder.readTimeout(10, TimeUnit.SECONDS).connectTimeout(10, TimeUnit.SECONDS).writeTimeout(10, TimeUnit.SECONDS).build()
+            );
         }
 
         public Response post(String id, String token, JSONObject message) throws RequestingException {
-            return newRequest(String.format(API_ENDPOINT, id, token), id).body(message).header("Content-Type", "application/json").post();
+            Request req = newRequest(String.format(API_ENDPOINT, id, token), id).body(message).header("Content-Type", "application/json");
+            Response res;
+            int i = 0;
+            do {
+                i++;
+                res = req.post();
+                if(res.code() == 429) {
+                    long tryAgainIn = res.asObject().getInt("retry_after");
+                    if(!onRateLimited(req, tryAgainIn)) {
+                        throw new RateLimitedException(tryAgainIn);
+                    }
+                }
+            } while(res.code() != 204 && i < 4);
+
+            if(!configured && res.code() == 204) {
+                int limit = Integer.parseInt(res.headers().get("x-ratelimit-limit").get(0));
+                int remaining = Integer.parseInt(res.headers().get("x-ratelimit-remaining").get(0));
+                if(remaining == limit-1) {
+                    long time = (Long.parseLong(res.headers().get("x-ratelimit-reset").get(0))- OffsetDateTime.now().toEpochSecond())/10;
+                    setRateLimiter(new RateLimiter(remaining, (int)(time)));
+                    configured = true;
+                }
+            }
+            return res;
         }
     }
 }
